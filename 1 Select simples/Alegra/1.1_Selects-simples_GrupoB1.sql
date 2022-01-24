7d0ce4033859169230667c090349c895581ae884
Jesús Egido <jesus.egido@idesie.com>
2022-01-24 17:27:37 +0100

    26

--- 1 Select simples/Alegra/1.1_Selects-simples_GrupoB1.sql
+++ 1 Select simples/Alegra/1.1_Selects-simples_GrupoB1.sql
@@ -135,7 +135,14 @@ Nombre, fecha de instalación, fecha de garantia
 de los componentes del facility 1
 que tienen fecha de garantia
 */
-
+select
+    name,
+    installatedon,
+    warrantystarton,
+    facilityid
+from components
+where facilityid = 1
+    and warrantystarton is not null;
 /* 27
 Lista de nombres de espacio que su id no es 4, 9, ni 19
 del floorid 1
