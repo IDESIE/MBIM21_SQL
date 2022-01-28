6784ec9961270dafac82dd8124fcb1a2892fadee
Marta <marta.tello@idesie.com>
2022-01-28 09:55:30 +0100

    14

--- 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
+++ 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
@@ -122,7 +122,13 @@ Pasi 4
 Cuántos componentes de instalaron un Jueves
 en el facilityid 1
 */
-
+select 
+ name,
+ to_char(installatedon,'Day'),
+ facilityid
+ from components 
+where rtrim(to_char(installatedon,'Day'))= 'Jueves'
+and facilityid= 1;
 /*15
 Listar el id de planta concatenado con un guión
 seguido del id de espacio concatenado con un guión
