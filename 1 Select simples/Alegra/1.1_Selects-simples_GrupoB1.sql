e0efb80dc041af0b628c5aa824b1f69589e01aa2
Jesús Egido <jesus.egido@idesie.com>
2022-01-30 18:22:34 +0100

    Update 1.1_Selects-simples_GrupoB1.sql

--- 1 Select simples/Alegra/1.1_Selects-simples_GrupoB1.sql
+++ 1 Select simples/Alegra/1.1_Selects-simples_GrupoB1.sql
@@ -11,54 +11,99 @@ Describir la tabla spaces
 DESC spaces;
 /* 3
 Datos de la tabla components
-
+DESC components;
 /* 4
 Datos de la tabla component_types
 */
-
+DESC components_types;
 /* 5
 Id, nombre de los facilities
 */
-
+select 
+    id,
+    name
+from components;
 /* 6
 Nombre, elevación e id del facility de las plantas
 */
-
+select
+    name,
+    elevation,
+    id
+from floors;
 /* 7
 Nombre, area bruta, volumen de los espacios
 */
-
+select 
+    name,
+    grossarea,
+    volume
+from spaces;
 /* 8
 Nombre, vida útil de los tipos de componentes del facility 1
 */
-
+select
+    name,
+    updatedat
+from components
+where facilityid=1;
 /* 9
 Nombre de los espacios de la Planta 1 del facility 1
 */
 /*Previamente se consulta cuál es el floorid
 listando los */
-
+select
+    name
+from spaces
+where floorid=1;
 /* 10
 Nombre, número de modelo del tipo de componente con id = 60
 */
-
+select
+    name,
+    serialnumber
+from components
+where id= 60;
 /* 11
 Nombre y fecha de instalación de los componentes del espacio 60 ordenados descendentemente por la fecha de instalación
 */
-
+select
+    name,
+    installatedon
+from components
+where spaceid= 60
+order by installatedon desc;
 /* 12
 Listar las distintas fechas de instalación de los componentes del facility 1 ordenados descendentemente.
 */
-
+select
+    name,
+    installatedon
+from components
+where facilityid= 1
+order by installatedon desc;
 /* 13
 Listar los distintos GUIDs de los componentes del facility 1 ordenados ascendentemente por fecha de garantía.
 */
-
+select
+    externalidentifier,
+    warrantystarton
+from components
+where facilityid= 1
+order by warrantystarton asc;
 /* 14
 Id, código de activo, GUID, número de serie y nombre de los componentes cuyo spaceid está entre 10 y 27 inclusive
 ordenados por id de espacio descendentemente.
 */
-
+select
+    id,
+    assetidentifier,
+    externalidentifier,
+    name,
+    spaceid
+from components
+where spaceid between 10 and 27
+order by spaceid desc;
 /* 15
 Id, código de activo, GUID, número de serie y nombre de los componentes del facility 1 
 ordenados por código de activo descendentemente.
@@ -77,7 +122,11 @@ order by assetidentifier desc;
 Códigos de activo de los componentes del espacio con id 21
 ordenados por código de activo descendentemente.
 */
-
+select
+    assetidentifier
+from components
+where spaceid = 21
+order by assetidentifier desc;
 /* 17
 Las distintas fechas de instalación de los componentes 
 de los espacios con id 10, 12, 16, 19 
@@ -94,12 +143,21 @@ Nombre, volumen, de los espacios
 cuyo volumen es mayor a 90 de floorid = 1
 ordenados por volumen descendentemente
 */
-
+select
+    name,
+    volume
+from spaces
+where floorid=1 and volume > 90
+order by volume desc;
 /* 19
 Nombre, volumen de los espacios
 cuyo volumen es mayor a 6 y menor a 9 de la planta con id = 1
 */
-
+select
+    name,
+    volume
+from spaces
+where floorid=1 and volume between 6 and 9;
 /* 20
 Nombre, código de activo, número de serie de los componentes
 que no tengan espacio del facility 1
