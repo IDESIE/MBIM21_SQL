c5b08ea25e55c4d8ae4c18e03d9b81de6643144c
Raquel Marco <raquel.marco@idesie.com>
2022-02-02 19:16:08 +0100

    Update 4.1_DDL_GrupoA1.sql
    TABLAS

--- 4 DDL/4.1_DDL_GrupoA1.sql
+++ 4 DDL/4.1_DDL_GrupoA1.sql
@@ -15,18 +15,44 @@ guid
 name
 description
 category
-address
+address*/
 
-FLOORS
+CREATE TABLE cb_facilities (
+    id number,
+    guid varchar2(4000),
+    name varchar2(4000) not null,
+    description varchar2(4000),
+    category varchar2(4000),
+    address varchar2(4000),
+CONSTRAINT pk_facili_id PRIMARY KEY (id),
+CONSTRAINT uq_facili_guid UNIQUE (guid),
+CONSTRAINT uq_facili_name UNIQUE (name)
+);
+
+/*FLOORS
 id
 guid
 name
 category
 description
 height
-facilityId
+facilityId*/
 
-SPACES
+CREATE TABLE cb_floors (
+    id number,
+    guid varchar2(4000),
+    name varchar2(4000) not null,
+    category varchar2(4000),
+    description varchar2(4000),
+    height number,
+    facilityid number not null,
+CONSTRAINT pk_cbfloors_id PRIMARY KEY (id),
+CONSTRAINT uq_cbfloors_guid UNIQUE (guid),
+CONSTRAINT uq_cbfloors_name UNIQUE (name),
+CONSTRAINT fk_cbfloors_facili FOREIGN KEY (facilityid) REFERENCES cb_facilities(id)
+);
+
+/*SPACES
 id
 guid
 name
@@ -34,9 +60,24 @@ category
 description
 usableHeight
 area
-floorId
+floorId*/
 
-COMPONENTS
+CREATE TABLE cb_spaces (
+    id number,
+    guid varchar2(4000),
+    name varchar2(4000) not null,
+    category varchar2(4000),
+    description varchar2(4000),
+    usableHeight number,
+    area number,
+    floorId number not null,
+CONSTRAINT pk_cbspaces_id PRIMARY KEY (id),
+CONSTRAINT uq_cbspaces_guid UNIQUE (guid),
+CONSTRAINT uq_cbspaces_name UNIQUE (name),
+CONSTRAINT fk_cbspaces_floorid FOREIGN KEY (floorid) REFERENCES cb_floors(id)
+);
+
+/*COMPONENTS
 id
 guid
 name
@@ -44,19 +85,47 @@ description
 serialNumber
 installatedOn
 spaceId
-typeId
+typeId*/
 
-TYPES
+CREATE TABLE cb_components (
+    id number,
+    guid varchar2(4000),
+    name varchar2(4000) not null,
+    description varchar2(4000),
+    serialNumber varchar(4000),
+    installatedOn date default sysdate,
+    spaceId varchar2(4000),
+    typeId number not null,
+CONSTRAINT pk_cbcomponents_id PRIMARY KEY (id),
+CONSTRAINT uq_cbcomponents_guid UNIQUE (guid),
+CONSTRAINT uq_cbcomponents_name UNIQUE (name),
+CONSTRAINT fk_cbcomponents_typeid FOREIGN KEY (typeId) REFERENCES cb_componenttype(id)
+);
+
+/*TYPES
 id
 guid
 name
 description
 modelNumber
 color
-warrantyYears
+warrantyYears*/
 
+CREATE TABLE cb_componenttype (
+    id number, 
+    guid varchar2(4000),
+    name varchar2(4000) not null,
+    description varchar2(4000),
+    modelNumber varchar2(4000),
+    color varchar2(4000),
+    warrantyYears number,
+CONSTRAINT pk_cbcomptype_id PRIMARY KEY (id),
+CONSTRAINT uq_cbcomptype_guid UNIQUE (guid),
+CONSTRAINT uq_cbcomptype_name UNIQUE (name),
+CONSTRAINT ck_cbomptype_warr CHECK (warrantyYears > 0)
+);
 
-En las definiciones establacer las siguientes restricciones
+/*En las definiciones establacer las siguientes restricciones
 -Los guid deben ser únicos.
 -No es posible dar de alta un componente sin un tipo.
 -No es posible dar de alta un espacio sin una planta.
