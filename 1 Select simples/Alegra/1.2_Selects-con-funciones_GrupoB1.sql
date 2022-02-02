1e12c36aca8a9cd24d224532f457f290f92c265a
Jesús Egido <jesus.egido@idesie.com>
2022-02-02 17:26:41 +0100

    1.2 - 3

--- 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB1.sql
+++ 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB1.sql
@@ -20,12 +20,23 @@ from components
 Día en palabras de cuando se instalaron los componentes
 del facility 1
 */
-
+select
+    to_char(installatedon,'Day')
+from components
+where facilityid=1
+;
 /* 3
 De los espacios, obtener la suma de áreas, cuál es el mínimo, el máximo y la media de áreas
 del floorid 1. Redondeado a dos dígitos.
 */
-
+select
+    round(sum(grossarea),2) "Suma",
+    round(min(grossarea),2) "Mínimo",
+    round(max(grossarea),2)"Máximo",
+    round(avg(grossarea),2)"Media"
+from spaces
+where floorid=1
+;
 /* 4
 Listar el número de componentes que tienen indicado el espacio y el número de componentes total.
 del facility 1
