934d6fa77a512106016384fdfb3f3ae9fb02889f
Marta <marta.tello@idesie.com>
2022-02-06 23:46:25 +0100

    Update 1.2_Selects-con-funciones_GrupoB2.sql

--- 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
+++ 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
@@ -12,7 +12,9 @@ la palabra "de", el mes en minúscula en palabras, la palabra "de", el año en c
 finalizando con un punto. Luego la hora en formato 24h con minutos y segundos.
 Y de etiqueta del campo "Fecha actual".
 */
-
+select 
+to_char(sysdate,'Day, dd "de" month "de" yyyy.  hh24:mi:ss')"Fecha actual"
+from components;
 /* 2
 Día en palabras de cuando se instalaron los componentes
 del facility 1
