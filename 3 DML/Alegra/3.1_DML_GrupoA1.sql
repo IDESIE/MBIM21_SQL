14968f352c6343923cebc4ad6627a8f0cb3a2e72
BlancaFrutos <blanca.frutos@idesie.com>
2022-02-06 20:28:47 +0100

    Update 3.1_DML_GrupoA1.sql
    3.1 blanca

--- 3 DML/Alegra/3.1_DML_GrupoA1.sql
+++ 3 DML/Alegra/3.1_DML_GrupoA1.sql
@@ -65,6 +65,10 @@ update components
 Modificar la fecha de garantia para que sea igual a la fecha de instalación
 para todo componente que sea un grifo o lavabo del facility 1.
 */
+update components
+    SET installatedon = warrantystarton
+    where components.facilityid=1 
+    and (name like '%Grifo%' or name like '%Lavabo%');
 
 /* 5 R
 Anonimizar los datos personales: nombre, apellido, email, teléfono de los contactos
