623100e14d51f94609c83ca5ed7da8318c38116e
Filipe Fernandes <filipe.fernandes@idesie.com>
2022-02-07 12:34:36 +0100

    Update 1.2_Selects-con-funciones_GrupoB2.sql

--- 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
+++ 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoB2.sql
@@ -54,6 +54,12 @@ Mostrar tres medias que llamaremos:
 de los espacios del floorid 1
 Solo la parte entera, sin decimales ni redondeo.
 */
+Select
+round (avg(grossarea),0) Media,
+trunc((avg(grossarea)+min(grossarea))/2,0) Mediabaja,
+trunc((avg(grossarea)+max(grossarea))/2,0) Mediaalta
+from spaces
+where floorid = 1;
 
 /* 6
 Cuántos componentes hay, cuántos tienen fecha inicio de garantia, cuántos tienen espacio, y en cuántos espacios hay componentes
