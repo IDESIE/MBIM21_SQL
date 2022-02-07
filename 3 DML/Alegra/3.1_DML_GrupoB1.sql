aee2d6ff9e25830fe39f69af7957b37de5cc6185
Omarperez98 <omar.perez@idesie.com>
2022-02-07 16:32:45 +0100

    Ejercicio 5 DML

--- 3 DML/Alegra/3.1_DML_GrupoB1.sql
+++ 3 DML/Alegra/3.1_DML_GrupoB1.sql
@@ -93,3 +93,9 @@ para todo componente que sea un grifo o lavabo del facility 1.
 /* 5
 Anonimizar los datos personales: nombre, apellido, email, teléfono de los contactos
 */
+update contacts
+    set
+        givenname = null,
+        familyname = null,
+        phone = null,
+        email = concat('ANONIMO',ID);
