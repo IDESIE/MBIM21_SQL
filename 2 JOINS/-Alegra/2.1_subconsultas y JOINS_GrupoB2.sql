cad87441e1ae1900794ebe4f1d1a94d84cbb1897
Filipe Fernandes <filipe.fernandes@idesie.com>
2022-02-15 12:45:12 +0100

    Update 2.1_subconsultas y JOINS_GrupoB2.sql

--- 2 JOINS/-Alegra/2.1_subconsultas y JOINS_GrupoB2.sql
+++ 2 JOINS/-Alegra/2.1_subconsultas y JOINS_GrupoB2.sql
@@ -176,6 +176,14 @@ Componentes    Tipo   Espacio
 1   Mesa-profesor           Aula 3
 21  Mesa-cristal-redonda    Aula 12
 */
+select 
+name,
+count(*)as num from component_types
+from components
+JOIN component_types ON components.name = component_types.name
+where facilityid=1 and lower(component_types.name) like '%mesa%'
+group by name
+order by count(*) desc;
 
 /*
 8
